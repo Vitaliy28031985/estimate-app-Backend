4be442225f79aae3914047191f436cdaffbe128d
Vitaliy <94307584+Vitaliy28031985@users.noreply.github.com>
2024-02-06 17:13:03 +0200

    update estimate

--- controllers/estimates/index.js
+++ controllers/estimates/index.js
@@ -2,10 +2,12 @@ const getAll = require("./getAll");
 const add = require("./add");
 const getEstimatesById = require("./getEstimatesById");
 const removeEstimate = require("./removeEstimate");
+const update = require("./updateEstimate");
 
 module.exports = {
     getAll,
     getEstimatesById,
     add,
-    removeEstimate
+    removeEstimate,
+    update
 };
--- routes/api/estimates.js
+++ routes/api/estimates.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {getAll, getEstimatesById, add, removeEstimate} = require("../../controllers/estimates")
+const {getAll, getEstimatesById, add, removeEstimate, update} = require("../../controllers/estimates")
 const {auth} = require("../../middlewares")
 
 const router = express.Router();
@@ -7,6 +7,7 @@ const router = express.Router();
 router.get('/', auth, getAll);
 router.get('/:estimatesId', auth, getEstimatesById);
 router.post('/:projectId', auth, add);
+router.patch('/:projectId/:estimateId', auth, update)
 router.delete('/:projectId/:estimateId', auth, removeEstimate)
 
 module.exports = router;
