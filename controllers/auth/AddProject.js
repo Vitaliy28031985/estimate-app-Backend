55cd8b5215a8479ac826f808bdde67ae837977f7
Vitaliy <94307584+Vitaliy28031985@users.noreply.github.com>
2024-03-15 17:08:18 +0200

    addProject allowList

--- controllers/auth/AddProject.js
+++ controllers/auth/AddProject.js
@@ -1,16 +1,23 @@
 const {User} = require("../../models/user");
+const { Projects } = require("../../models/estimate");
 
 const addProject = async (req, res) => {
-   
+    const { _id } = req.user;
     const {projectId} = req.params; 
     const emailCurrent = req.body;
+
+    const project = await Projects.findById({ owner: _id, _id: projectId },
+        '-createdAt -updatedAt');
      
     const user = await User.find();
     const currentUser = user.filter(({email}) => email === emailCurrent.email);
     currentUser[0].projectIds.push(projectId);
+    project.allowList.push(currentUser[0]._id);
+    
+
  
     try {
-        
+    const addAllow = await Projects.findByIdAndUpdate(projectId, { $set: { allowList: project.allowList } },{ new: true })  
     const AddProjectId = await User.findByIdAndUpdate(currentUser[0]._id, { $set: { projectIds: currentUser[0].projectIds } },{ new: true });
     res.status(201).json(currentUser[0].projectIds);
     } catch (error) {
